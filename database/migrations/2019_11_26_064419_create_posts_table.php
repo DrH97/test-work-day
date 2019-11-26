da6069c14f83cceec903b57d26c1723da969a5e4
drh <jmnabangi@gmail.com>
2019-11-26 10:30:30 +0300

    Created posts migration and added foreign keys to users
    Signed-off-by: drh <jmnabangi@gmail.com>

--- database/migrations/2019_11_26_064419_create_posts_table.php
+++ database/migrations/2019_11_26_064419_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->integer('likes');
+            $table->integer('views');
             $table->timestamps();
         });
     }

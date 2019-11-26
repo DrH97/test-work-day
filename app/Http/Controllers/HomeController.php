e660aec3f8b3c9b323588e301a0e211a29a1d6c2
drh <jmnabangi@gmail.com>
2019-11-26 12:34:47 +0300

    Initial implementation of profile CRUD(view, edit, delete)
    Signed-off-by: drh <jmnabangi@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -28,5 +29,42 @@ class HomeController extends Controller
         $userPosts = Post::whereUserId(Auth::id())->get();
 
         return view('dashboard.dashboard', compact('userPosts'));
+    }
+
+    /**
+     * Show the user's profile.
+     *
+     * @return Renderable
+     */
+    public function showProfile()
+    {
+        $user = Auth::user();
+
+        return view('dashboard.profile', compact('user'));
+    }
+
+    /**
+     * Update the user's profile.
+     *
+     * @return Renderable
+     */
+    public function updateProfile(Request $request)
+    {
+//        $user = Auth::user();
+//        $user->name =
+
+        return redirect('dashboard/profile');
+    }
+
+    /**
+     * Delete the user's profile.
+     *
+     * @return Renderable
+     */
+    public function deleteProfile()
+    {
+        $user = Auth::user();
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ Route::get('/', 'PostController@index')->name('welcome');
 Auth::routes();
 
 Route::get('/dashboard', 'HomeController@index')->name('home');
+Route::get('/dashboard/profile', 'HomeController@showProfile')->name('profile');
+Route::post('/dashboard/profile', 'HomeController@updateProfile')->name('profile.edit');
+Route::delete('/dashboard/profile', 'HomeController@deleteProfile')->name('profile.destroy');
 
 Route::resource('posts', 'PostController');
 Route::post('/posts/{post}/like', 'PostController@like')->name('posts.like');

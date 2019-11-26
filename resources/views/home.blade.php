8088b15871f7e682b500f5539a9ab0554f45734c
drh <jmnabangi@gmail.com>
2019-11-26 10:05:10 +0300

    Initial file setup and commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+{{--                <img class="card-img-top" src="..." alt="Card image cap">--}}
+                <div class="card-body">
+                    <h5 class="card-title">Post title</h5>
+                    <p class="card-text">Short description of post...</p>
+                    <a href="#" class="btn btn-primary">View Post</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
